51974c1e3c79fac28ca86e6a7a8c3d8ed82476d4
Hoggspjot <vingisaar@gmail.com>
2025-07-29 17:23:56 +0300

    своя реализация ArrayList

---
+++ src/main/java/twoPart/TestString.java
@@ -0,0 +1,23 @@
+package twoPart;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestString {
+    public static void main(String[] args) {
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+
+        myArrayList.add("Hello");
+        myArrayList.add("World");
+        myArrayList.print();
+        System.out.println(myArrayList.getSize());
+        System.out.println(myArrayList.get(1));
+        myArrayList.remove(1);
+
+        List<String> list = Arrays.asList("Bamboleyo");
+
+        myArrayList.addAll(list);
+        myArrayList.print();
+    }
+
+}
