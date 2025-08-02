84175b51a4dddeaed0b8467cdf5e337f116b15c9
Hoggspjot <vingisaar@gmail.com>
2025-08-02 13:54:57 +0300

    своя реализация LinkedList финал

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,8 +1,9 @@
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public class MyLinkedList<E> implements Iterable<E> {
-    private int size = 0;
+    private int size;
     private Node<E> first;
     private Node<E> last;
 
@@ -22,35 +23,84 @@ public class MyLinkedList<E> implements Iterable<E> {
     }
 
     public void add(E element) {
-        if (size == 0) {
-            firstNode(element);
+        Node<E> l = last;
+        Node<E> newNode = new Node<>(element, l, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
         } else {
-            lastNode(element);
+            l.next = newNode;
         }
+        size++;
     }
 
-    public void add(E element , int index) {
+
+    public E get(int index) {
         checkIndex(index);
-        Node<E> tempNode;
-
-        for (int i = 0; i < size; i++) {
-
-        }
-
-        Node<E> newNode = new Node<>()
+        Node<E> node = getNode(index);
+        return node.value;
     }
 
+
+    private Node<E> getNode(int index) {
+        checkIndex(index);
+        Node<E> search = first;
+        for (int i = 0; i < index; i++) {
+            search = search.next;
+        }
+        return search;
+    }
+
+
+    public E remove(int index) {
+        checkIndex(index);
+        Node<E> removeNode;
+        E value;
+
+        if (index == 0) {
+            removeNode = first;
+            value = removeNode.value;
+            first = first.next;
+        } else if (index == size - 1) {
+            removeNode = last;
+            value = removeNode.value;
+            last = last.prev;
+            last.next = null;
+        } else {
+            removeNode = getNode(index);
+            Node<E> prev = removeNode.prev;
+            Node<E> next = removeNode.next;
+            prev.next = next;
+            next.prev = prev;
+        }
+
+        value =  removeNode.value;
+
+        removeNode.prev = null;
+        removeNode.next = null;
+        removeNode.value = null;
+
+        size--;
+        return value;
+    }
+
+
+    public void addAll(Collection<? extends E> collection) {
+        for (E value : collection) {
+            add(value);
+        }
+    }
 
 
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new LinkedListIterator();
     }
 
 
     private void checkIndex(int index) {
-        if (index < 0 || index > size) {
+        if (index < 0 || index >= size) {
             throw new ArrayIndexOutOfBoundsException("Выход за пределы массива");
         }
     }
@@ -66,9 +116,41 @@ public class MyLinkedList<E> implements Iterable<E> {
             this.next = next;
             this.prev = prev;
         }
+        private void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 
+    private class LinkedListIterator implements Iterator<E> {
 
+        private Node<E> iter = first;
 
+        @Override
+        public boolean hasNext() {
+            return iter != null;
+        }
 
+        @Override
+        public E next() {
+            if (iter == null) {
+                throw new NoSuchElementException();
+            }
+            E value = iter.value;
+            iter = iter.next;
+            return value;
+
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        Node<E> temp = first;
+        while (temp != null) {
+            sb.append(temp.value + " ");
+            temp = temp.next;
+        }
+        return sb.toString();
+    }
 }
